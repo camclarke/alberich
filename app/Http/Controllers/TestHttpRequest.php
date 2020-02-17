ef1c692e428e668a42be4f661e0ce271d31090ca
Israel Rocha <camclarke@live.com>
2020-02-17 19:46:26 -0400

    httprequeesttest

---
+++ app/Http/Controllers/TestHttpRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestHttpRequest extends Controller
+{
+    //
+    function index()
+    {
+        echo "Controller Created";
+    }
+}
